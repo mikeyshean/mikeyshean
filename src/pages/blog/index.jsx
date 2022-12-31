db32297d51916c8b9fdd39eabd5d8dee77453320
Mikey Shean <mdshean2@gmail.com>
2022-12-31 15:52:46 -0800

    Update krypton blog

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -38,15 +38,15 @@ export default function ArticlesIndex({ articles }) {
   return (
     <>
       <Head>
-        <title>Articles - Mikey Shean</title>
+        <title>Blog - Mikey Shean</title>
         <meta
           name="description"
           content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
         />
       </Head>
       <SimpleLayout
-        title="Writing on software design, company building, and the aerospace industry."
-        intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+        title="Writing on software engineering, web development, and the world of tech."
+        intro="A place for my thoughts on software engineering, web development, technical tutorials, and anything else."
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
