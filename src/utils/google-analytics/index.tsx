a735ef9a34c02f210ae167c7a7f79b30f923e274
Mikey Shean <mdshean2@gmail.com>
2023-01-04 22:37:56 -0800

    remove console.logs

--- src/utils/google-analytics/index.tsx
+++ src/utils/google-analytics/index.tsx
@@ -1,8 +1,6 @@
 import ReactGA4 from "react-ga4";
 const InitializeGoogleAnalytics = () => {
   ReactGA4.initialize(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS);
-
-  console.log("GA INITIALIZED");
 };
 
 const TrackGoogleAnalyticsEvent = (
@@ -10,7 +8,6 @@ const TrackGoogleAnalyticsEvent = (
   action: string,
   label: string
 ) => {
-  console.log("GA event:", category, ":", action, ":", label);
   // Send GA4 Event
   ReactGA4.event({
     category: category,
